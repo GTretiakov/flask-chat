f71c92f19564f9c66af4a7f77744f6028eb7687f
GTretiakov <g.tret1990@gmail.com>
2021-01-21 19:01:38 +0000

    initial commit

---
+++ run.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Main page with instructions"""
+    return "To send a message use /USERNAME/MESSAGE"
+
+@app.route('/<username>')
+def user(username):
+    return "hi " + username
+
+@app.route('/<username>/<message>')
+def send_message(username, message):
+    return "{0}: {1}".format(username, message)
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
