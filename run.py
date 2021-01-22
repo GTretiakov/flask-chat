00a624110aa3fcedefe6c2dd1263cfd66cd0b0e1
GTretiakov <g.tret1990@gmail.com>
2021-01-22 05:18:54 +0000

    add Procfile

--- run.py
+++ run.py
@@ -1,17 +1,16 @@
 import os
 from datetime import datetime
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
-app.secret_key = "randomstring123"
+app.secret_key = os.getenv("SECRET", "randomstring123")
 messages = []
 
-def add_messages(username, message):
+def add_message(username, message):
     now = datetime.now().strftime("%H:%M:%S")
-    messages.append("({}) {}:{}".format(now, username, message))
+    messages.append({"timestamp": now, "from": username, "message": message})
 
-def get_all_messages():
-    return "<br>".join(messages)
+
 
 @app.route('/', methods=["GET", "POST"])
 def index():
@@ -20,22 +19,22 @@ def index():
         session["username"] = request.form["username"]
 
     if "username" in session:
-        return redirect(session["username"])
+        return redirect(url_for("user", username=session["username"]))
     return render_template("index.html")
 
 
-@app.route('/<username>')
+@app.route('/chat/<username>', methods=["GET", "POST"])
 def user(username):
-    return "<h1>welcome, {}</h1>{}".format(username, get_all_messages())
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_message(username, message)
+        return redirect(url_for("user", username=session["username"]))
+    return render_template(
+        "chat.html", username=username, chat_messages=messages)
 
-@app.route('/<username>/<message>')
-def send_message(username, message):
-    add_messages(username, message)
-    return redirect("/" + username)
 
-
-if __name__ == "__main__":
-    app.run(
-        host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", "5000")),
-        debug=True)
+app.run(
+    host=os.environ.get("IP", "0.0.0.0"),
+    port=int(os.environ.get("PORT", "5000")),
+    debug=True)
